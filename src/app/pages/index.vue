0d3b80dd22ab08c1be4295b29b09218162b8bd4f
gangan <44604921+shinGangan@users.noreply.github.com>
2025-01-11 16:46:10 +0900

    exp(ui): pagesに全部書く

--- src/app/pages/index.vue
+++ src/app/pages/index.vue
@@ -1,24 +1,55 @@
+<script setup lang="ts">
+import type { InferOutput } from 'valibot';
+import { pipe, object, string, regex, maxLength, union, literal } from 'valibot';
+import { useDateFormat, useNow } from '@vueuse/core';
+import type { TableColumn } from '#ui/types';
+
+/**
+ * @param id - ユーザーID
+ * @param name - 顧客名
+ * @param phone - 電話番号
+ * @param createdDate - 登録日. YYYY/MM/DD 形式で表示する
+ * @param status - 顧客ステータス. available: 利用可能, unavailable: 利用不可
+ */
+const UserSchema = object({
+  id: string(),
+  name: string(),
+  phone: pipe(
+    string(),
+    maxLength(12, '電話番号を正しく入力してください'),
+    regex(/^([0][0-9]{1,4})[-]?[0-9]{1,4}[-]?[0-9]{4}$/, '電話番号を正しく入力してください')
+  ),
+  createdDate: string(),
+  status: union([literal('available'), literal('unavailable')])
+});
+type UserTable = InferOutput<typeof UserSchema>;
+
+interface UserTableColumn extends TableColumn {
+  key: keyof UserTable;
+  label: string;
+}
+
+const columns: UserTableColumn[] = [
+  { key: 'id', label: '顧客ID' },
+  { key: 'name', label: '顧客名' },
+  { key: 'phone', label: '電話番号' },
+  { key: 'createdDate', label: '登録日' },
+  { key: 'status', label: 'ステータス' }
+];
+
+const rows: UserTable[] = [...Array(15)].map((v, i) => {
+  return {
+    id: i + 1,
+    name: `テストユーザー${i + 1}`,
+    phone: `090-1234-56${String(i + 1).padStart(2, '0')}`,
+    createdDate: useDateFormat(useNow(), 'YYYY-MM-DD'),
+    status: i % 3 ? 'available' : 'unavailable'
+  };
+});
+</script>
+
 <template>
-  <UContainer>
-    <UCard class="mt-10">
-      <template #header>
-        <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme>
-            <USelect
-              v-model="$colorMode.preference"
-              :options="['system', 'light', 'dark']"
-            />
-          </ColorScheme>
-        </div>
-      </template>
-      <UButton
-        icon="i-heroicons-book-open"
-        to="https://ui.nuxt.com"
-        target="_blank"
-      >
-        Open Nuxt UI Documentation
-      </UButton>
-    </UCard>
+  <UContainer class="flex justify-center pt-16">
+    <UTable :rows :columns />
   </UContainer>
 </template>
