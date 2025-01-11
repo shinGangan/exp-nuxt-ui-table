db47c63c91cd6edd82e4812fb55f783edb15bc2c
gangan <44604921+shinGangan@users.noreply.github.com>
2025-01-11 17:36:24 +0900

    exp(ui): i18n custom block, ui props, onClicked row

--- src/app/pages/index.vue
+++ src/app/pages/index.vue
@@ -4,6 +4,8 @@ import { pipe, object, string, regex, maxLength, union, literal } from 'valibot'
 import { useDateFormat, useNow } from '@vueuse/core';
 import type { TableColumn } from '#ui/types';
 
+const { t } = useI18n();
+
 /**
  * @param id - ユーザーID
  * @param name - 顧客名
@@ -29,27 +31,66 @@ interface UserTableColumn extends TableColumn {
   label: string;
 }
 
+// TODO: UserTableColumn.key配列を生成し、それをループで回せたら嬉しいね
 const columns: UserTableColumn[] = [
-  { key: 'id', label: '顧客ID' },
-  { key: 'name', label: '顧客名' },
-  { key: 'phone', label: '電話番号' },
-  { key: 'createdDate', label: '登録日' },
-  { key: 'status', label: 'ステータス' }
+  { key: 'id', label: t('table.id') },
+  { key: 'name', label: t('table.name') },
+  { key: 'phone', label: t('table.phone') },
+  { key: 'createdDate', label: t('table.createdDate') },
+  { key: 'status', label: t('table.status') }
 ];
 
-const rows: UserTable[] = [...Array(15)].map((v, i) => {
-  return {
-    id: i + 1,
-    name: `テストユーザー${i + 1}`,
-    phone: `090-1234-56${String(i + 1).padStart(2, '0')}`,
-    createdDate: useDateFormat(useNow(), 'YYYY-MM-DD'),
-    status: i % 3 ? 'available' : 'unavailable'
-  };
-});
+const rows: UserTable[]
+  = [...Array(15)].map((_, i) => {
+    const id = i + 1;
+    return {
+      id,
+      name: `テストユーザー${id}`,
+      phone: `090-1234-56${String(id).padStart(2, '0')}`,
+      createdDate: useDateFormat(useNow(), 'YYYY-MM-DD'),
+      status: i % 3 ? 'available' : 'unavailable'
+    };
+  });
+
+/**
+ * テーブルの各行を押下した際, アラートを表示する
+ * @param row
+ */
+const selectRow = (row: UserTable): void => {
+  alert(`${row.name}は${t(`status.${row.status}`)}ユーザーです.`);
+};
 </script>
 
 <template>
-  <UContainer class="flex justify-center pt-16">
-    <UTable :rows :columns />
+  <UContainer class="pt-16">
+    <UTable
+      :rows
+      :columns
+      :ui="{
+        th: { base: 'text-center' },
+        tr: { base: 'text-center hover:cursor-pointer' }
+      }"
+      @select="selectRow"
+    >
+      <template #status-data="{ row }">
+        <UBadge
+          :label="t(`status.${row.status}`)"
+          :color="row.status === 'available' ? 'primary' : 'gray'"
+        />
+      </template>
+    </UTable>
   </UContainer>
 </template>
+
+<i18n lang="yaml">
+ja:
+  table:
+    id: 顧客ID
+    name: 顧客名
+    phone: 電話番号
+    createdDate: 登録日
+    status: ステータス
+  status:
+    available: 利用可能
+    unavailable: 利用不可
+</i18n>
